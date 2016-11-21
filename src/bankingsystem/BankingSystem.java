a49ffdc637c10d3a3b5f6d8542317f79a869e34b
esraaHosny <esraa_93_hosny@hotmail.com>
2016-11-22 00:35:47 +0300

    first commit

---
+++ src/bankingsystem/BankingSystem.java
@@ -0,0 +1,11 @@
+package bankingsystem;
+public class BankingSystem {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bank b = new Bank();
+        
+    }
+    
+    
+}
